9ef9d1cca23dd4af45611742694f542a62caf8c3
Behroozi0149 <tahabehroozi298@gmail.com>
2024-12-14 20:27:44 +0330

    add a readme file

--- Novin.Libary.Backend.API/Program.cs
+++ Novin.Libary.Backend.API/Program.cs
@@ -1,7 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Novin.Libary.Backend.API.DB;
 using Novin.Libary.Backend.API.Entities;
-
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -26,37 +25,85 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 app.UseHttpsRedirection();
-// Book list
+// Book List
 app.MapGet("/books/list", (FirstDB db) =>
 {
     return db.Books.ToList();
 });
-// Book add
+// Book Add
 app.MapPost("/books/add", (FirstDB db, Book book) =>
 {
     db.Books.Add(book);
     db.SaveChanges();
 });
-// Member list
+// Book Edit 
+app.MapPost("/books/edit", (FirstDB db, Book book) =>
+{
+    db.Books.Update(book);
+    db.SaveChanges();
+});
+// Book Remove
+app.MapPost("/books/remove/{id}", (FirstDB db, int id) =>
+{
+    var book = db.Books.Find(id);
+    if (book != null)
+    {
+        db.Books.Remove(book);
+        db.SaveChanges();
+    }
+});
+// Member List
 app.MapGet("/members/list", (FirstDB db) =>
 {
     return db.Members.ToList();
 });
-// Member add
+// Member Add
 app.MapPost("/members/add", (FirstDB db, Member member) =>
 {
     db.Members.Add(member);
     db.SaveChanges();
 });
-// Borrow list
+// Member Edit 
+app.MapPost("/members/edit", (FirstDB db, Member member) =>
+{
+    db.Members.Update(member);
+    db.SaveChanges();
+});
+// Member Remove
+app.MapPost("/members/remove/{id}", (FirstDB db, int id) =>
+{
+    var member = db.Members.Find(id);
+    if (member != null)
+    {
+        db.Members.Remove(member);
+        db.SaveChanges();
+    }
+});
+// Borrow List
 app.MapGet("/borrow/list", (FirstDB db) =>
 {
     return db.Borrows.Include(m => m.Book).Include(m => m.Member).ToList();
 });
-// Borrow add
+// Borrow Add
 app.MapPost("/borrows/add", (FirstDB db, Borrow borrow) =>
 {
     db.Borrows.Add(borrow);
     db.SaveChanges();
+});
+// Borrow Edit 
+app.MapPost("/borrows/edit", (FirstDB db, Borrow borrow) =>
+{
+    db.Borrows.Update(borrow);
+    db.SaveChanges();
+});
+// Borrow Remove
+app.MapPost("/borrows/remove/{id}", (FirstDB db, int id) =>
+{
+    var borrow = db.Borrows.Find(id);
+    if (borrow != null)
+    {
+        db.Borrows.Remove(borrow);
+        db.SaveChanges();
+    }
 });
 app.Run();
