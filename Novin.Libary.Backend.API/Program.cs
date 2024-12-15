900fafbd4ce3534907b044c244574ab2ef747c00
Behroozi0149 <tahabehroozi298@gmail.com>
2024-12-15 19:13:02 +0330

    change a codes in program.cs

--- Novin.Libary.Backend.API/Program.cs
+++ Novin.Libary.Backend.API/Program.cs
@@ -26,16 +26,16 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 app.UseHttpsRedirection();
-// Book List
-app.MapGet("/books/list", (FirstDB db) =>
-{
-    return db.Books.ToList();
-});
 // Book Add
 app.MapPost("/books/add", (FirstDB db, Book book) =>
 {
     db.Books.Add(book);
     db.SaveChanges();
+});
+// Book List
+app.MapGet("/books/list", (FirstDB db) =>
+{
+    return db.Books.ToList();
 });
 // Book Edit    Exersice
 app.MapPost("/books/edit", (FirstDB db, Book book) =>
@@ -53,16 +53,16 @@ app.MapPost("/books/remove/{id}", (FirstDB db, int id) =>
         db.SaveChanges();
     }
 });
-// Member List
-app.MapGet("/members/list", (FirstDB db) =>
-{
-    return db.Members.ToList();
-});
 // Member Add
 app.MapPost("/members/add", (FirstDB db, Member member) =>
 {
     db.Members.Add(member);
     db.SaveChanges();
+});
+// Member List
+app.MapGet("/members/list", (FirstDB db) =>
+{
+    return db.Members.ToList();
 });
 // Member Edit    Exersice
 app.MapPost("/members/edit", (FirstDB db, Member member) =>
@@ -80,16 +80,16 @@ app.MapPost("/members/remove/{id}", (FirstDB db, int id) =>
         db.SaveChanges();
     }
 });
-// Borrow List
-app.MapGet("/borrow/list", (FirstDB db) =>
-{
-    return db.Borrows.Include(m => m.Book).Include(m => m.Member).ToList();
-});
 // Borrow Add
 app.MapPost("/borrows/add", (FirstDB db, Borrow borrow) =>
 {
     db.Borrows.Add(borrow);
     db.SaveChanges();
+});
+// Borrow List
+app.MapGet("/borrow/list", (FirstDB db) =>
+{
+    return db.Borrows.Include(m => m.Book).Include(m => m.Member).ToList();
 });
 // Borrow Edit    Exersice
 app.MapPost("/borrows/edit", (FirstDB db, Borrow borrow) =>
