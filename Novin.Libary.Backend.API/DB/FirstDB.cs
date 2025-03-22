216570d246724f25d66a47e371e2379b9b07c74c
Behroozi0149 <tahabehroozi298@gmail.com>
2025-03-22 16:38:13 +0330

    update

--- Novin.Libary.Backend.API/DB/FirstDB.cs
+++ Novin.Libary.Backend.API/DB/FirstDB.cs
@@ -2,12 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Novin.Libary.Backend.API.Entities;
 
 namespace Novin.Libary.Backend.API.DB
 {
-    public class FirstDB : DbContext
+    public class FirstDB : IdentityDbContext<LibraryUser>
     {
         public required DbSet<Book> Books { get; set; }
         public required DbSet<Borrow> Borrows { get; set; }
---
+++ Novin.Libary.Backend.API/Entities/LibraryUser.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Novin.Libary.Backend.API.Entities
+{
+    public class LibraryUser:IdentityUser
+    {
+        public string? fullname { get; set; }
+    }
+}
