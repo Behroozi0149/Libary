c2a2591303a435393821c979b1e6f3da54eb4132
Behroozi0149 <tahabehroozi298@gmail.com>
2025-04-01 21:10:01 +0330

    update...

--- novin.Libary.Frontend/src/main.ts
+++ novin.Libary.Frontend/src/main.ts
@@ -4,3 +4,16 @@ import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+console.log('WELCOME');
+console.log('SUPPORT:tahabehroozi0149@gmail.com');
+console.log('INSTAGRAM:taha_behroozi_');
+console.log('TELEGRAM:dev_taha_behroozi');
+// change title
+const titleTag = document.title;
+window.addEventListener("blur", function () {
+  document.title = "Library management | Waiting";
+});
+window.addEventListener("focus", function () {
+  document.title = titleTag;
+});
